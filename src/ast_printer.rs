a75fc54238a4a3e04ddaff766d1352b4c564b01b
安逐悲 <nayamior@outlook.com>
2024-06-23 18:01:13 +0800

    finish representing code

--- src/ast_printer.rs
+++ src/ast_printer.rs
@@ -1,4 +1,4 @@
-use crate::expr::{BinaryExpr, Expr, GroupingExpr, LiteralExpr, UnaryExpr, Visitor};
+use crate::expr::{Expr, Visitor};
 use crate::token::Literal;
 
 pub struct ExprVisitor;
@@ -19,50 +19,27 @@ impl ExprVisitor {
     }
 }
 
-// TODO: 用模式匹配把visit_binary这些函数都换成模式匹配里的代码块，合并到一个函数里
 impl Visitor<String> for ExprVisitor {
     fn visit(&self, expr: &Expr) -> String {
         match expr {
-            Expr::Binary(binary_expr) => self.parenthesize(
-                binary_expr.operator.lexeme.as_str(),
-                vec![&binary_expr.left, &binary_expr.right],
+            Expr::Binary {
+                left,
+                operator,
+                right,
+            } => self.parenthesize(
+                operator.lexeme.as_str(),
+                vec![left.as_ref(), right.as_ref()],
             ),
-            Expr::Grouping(grouping_expr) => {
-                self.parenthesize("group", vec![&grouping_expr.expression])
-            }
-            Expr::Literal(literal_expr) => {
-                if let Literal::Nil = literal_expr.value {
+            Expr::Grouping { expression } => self.parenthesize("group", vec![expression.as_ref()]),
+            Expr::Literal { value } => {
+                if let Literal::Nil = value {
                     return String::from("nil");
                 }
-                literal_expr.value.to_string()
+                value.to_string()
             }
-            Expr::Unary(unary_expr) => {
-                self.parenthesize(unary_expr.operator.lexeme.as_str(), vec![&unary_expr.right])
+            Expr::Unary { operator, right } => {
+                self.parenthesize(operator.lexeme.as_str(), vec![right.as_ref()])
             }
         }
     }
 }
-
-// impl crate::expr::Visitor<String> for ExprVisitor {
-//     fn visit_binary(&self, binary_expr: &BinaryExpr) -> String {
-//         self.parenthesize(
-//             binary_expr.operator.lexeme.as_str(),
-//             vec![&binary_expr.left, &binary_expr.right],
-//         )
-//     }
-
-//     fn visit_grouping(&self, grouping_expr: &GroupingExpr) -> String {
-//         self.parenthesize("group", vec![&grouping_expr.expression])
-//     }
-
-//     fn visit_literal(&self, literal_expr: &LiteralExpr) -> String {
-//         if let Literal::Nil = literal_expr.value {
-//             return String::from("nil");
-//         }
-//         literal_expr.value.to_string()
-//     }
-
-//     fn visit_unary(&self, unary_expr: &UnaryExpr) -> String {
-//         self.parenthesize(unary_expr.operator.lexeme.as_str(), vec![&unary_expr.right])
-//     }
-// }
