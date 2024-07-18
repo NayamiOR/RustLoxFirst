9e73c524ab57307548e8011626bf74ba9ceddd04
Nayami <nayamior@outlook.com>
2024-07-18 15:05:40 +0800

    update ast_printer.rs

--- src/ast_printer.rs
+++ src/ast_printer.rs
@@ -20,26 +20,37 @@ impl ExprVisitor {
 }
 
 impl Visitor<String> for ExprVisitor {
-    fn visit(&self, expr: &Expr) -> String {
-        match expr {
-            Expr::Binary {
-                left,
-                operator,
-                right,
-            } => self.parenthesize(
-                operator.lexeme.as_str(),
-                vec![left.as_ref(), right.as_ref()],
-            ),
-            Expr::Grouping { expression } => self.parenthesize("group", vec![expression.as_ref()]),
-            Expr::Literal { value } => {
-                if let Literal::Nil = value {
-                    return String::from("nil");
-                }
-                value.to_string()
-            }
-            Expr::Unary { operator, right } => {
-                self.parenthesize(operator.lexeme.as_str(), vec![right.as_ref()])
-            }
-        }
+    fn visit_binary(&self, left: &Expr, operator: &crate::token::Token, right: &Expr) -> String {
+        self.parenthesize(operator.lexeme.as_str(), vec![left, right])
     }
+
+    fn visit_grouping(&self, expression: &Expr) -> String {
+        self.parenthesize("group", vec![expression])
+    }
+
+    fn visit_literal(&self, value: &crate::token::Literal) -> String {
+        if let Literal::Nil = value {
+            return String::from("nil");
+        }
+        value.to_string()
+    }
+
+    fn visit_unary(&self, operator: &crate::token::Token, right: &Expr) -> String {
+        self.parenthesize(operator.lexeme.as_str(), vec![right])
+    }
+}
+
+pub fn print(expr: &Expr) -> String {
+    expr.accept(&ExprVisitor)
+}
+
+fn parenthesize(name: &str, exprs: Vec<&Expr>) -> String {
+    let mut result = String::from("(");
+    result.push_str(name);
+    for expr in exprs {
+        result.push(' ');
+        result.push_str(&expr.accept(&ExprVisitor));
+    }
+    result.push(')');
+    result
 }
